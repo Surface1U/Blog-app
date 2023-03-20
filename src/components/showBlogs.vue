b70f26c8a00118d838066a1f7ea1d279efbeadd5
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-21 00:04:51 +0300

    added search of blogs

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,9 +1,10 @@
 <template>
-    <div id = "show-blogs">
+    <div v-theme:column = "'narrow'" id = "show-blogs">
         <h1>All Blog Articles</h1>
-        <div v-for="blog in blogs" class = "single-blog">
-            <h2>{{ blog.title }}</h2>
-            <article>{{ blog.body }}</article>
+        <input type="text" v-model="search" placeholder="search blogs"/>
+        <div v-for="blog in filteredBlogs" class = "single-blog">
+            <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+            <article>{{ blog.body | snippet }}</article>
         </div>
     </div>
   </template>
@@ -17,7 +18,8 @@
 
     data() {
       return{
-        blogs: []
+        blogs: [],
+        search:'abc'
       }
     },
     methods: {
@@ -28,6 +30,14 @@
             console.log(data);
            this.blogs = data.body.slice(0,10);
         })
+    },
+
+    computed: {
+        filteredBlogs: function(){
+            return this.blogs.filter((blog) => {
+                return blog.title.match(this.search);
+            })
+        }
     }
   }
 

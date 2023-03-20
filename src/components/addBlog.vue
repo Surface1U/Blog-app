2475f3aeb80771e05a696f3675a2c12b99e1c3cc
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-03-20 21:43:00 +0300

    Update addBlog.vue

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -39,7 +39,7 @@
             <p>Author: {{ blog.author }}</p>
         </div>
     </div>
-  </template>[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=your-github-username)](https://github.com/anuraghazra/github-readme-stats)
+  </template>
 
 
 
